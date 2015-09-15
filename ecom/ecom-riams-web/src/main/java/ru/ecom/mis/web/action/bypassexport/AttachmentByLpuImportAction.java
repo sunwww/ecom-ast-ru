f5194f50a7cae932153b2d93a614b98bc9cb68ce
vasek00786 <vasek00786@users.noreply.github.com>
2015-09-15 16:30:01 +0300

    Импорт прикрепленного населения

--- ecom/ecom-riams-web/src/main/java/ru/ecom/mis/web/action/bypassexport/AttachmentByLpuImportAction.java
+++ ecom/ecom-riams-web/src/main/java/ru/ecom/mis/web/action/bypassexport/AttachmentByLpuImportAction.java
@@ -1,28 +1,17 @@
 package ru.ecom.mis.web.action.bypassexport;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.upload.FormFile;
 
 import ru.ecom.ejb.services.monitor.IRemoteMonitorService;
-import ru.ecom.ejb.services.query.WebQueryResult;
 import ru.ecom.expomc.ejb.services.form.importformat.IImportFormatService;
-import ru.ecom.expomc.ejb.services.importservice.ImportException;
 import ru.ecom.expomc.ejb.services.importservice.ImportFileForm;
-import ru.ecom.mis.ejb.service.addresspoint.IAddressPointService;
-import ru.ecom.web.util.ActionUtil;
+import ru.ecom.expomc.ejb.services.sync.ISyncService;
 import ru.ecom.web.util.FileUploadUtil;
 import ru.ecom.web.util.Injection;
 import ru.nuzmsh.util.format.DateFormat;
@@ -30,45 +19,44 @@ import ru.nuzmsh.web.struts.BaseAction;
 
 public class AttachmentByLpuImportAction extends BaseAction {
     public ActionForward myExecute(ActionMapping aMapping, ActionForm aForm, HttpServletRequest aRequest, HttpServletResponse aResponse) throws Exception {
-    	AttachmentByLpuForm form =(AttachmentByLpuForm)aForm ;
-        System.out.println("00000000000 = 01");
+    	final AttachmentByLpuForm form =(AttachmentByLpuForm)aForm ;
+    	final long fileId = 0;
+    	 IRemoteMonitorService monitorService = (IRemoteMonitorService) Injection.find(aRequest).getService("MonitorService") ;
+	     
+    	final long monitorId = monitorService.createMonitor();
+       // System.out.println("00000000000 = 01");
     	if (form!=null ) {
-    		System.out.println("00000000000 = 02");	
+    	//	System.out.println("00000000000 = 02");	
     		FormFile formFile = form.getAttachmentFile();
     	
     		if (formFile!=null&&!formFile.equals("")) {
-    			System.out.println("00000000000 = 03");
-    			System.out.println("FormFile="+formFile.getFileSize());
+    	//		System.out.println("00000000000 = 03");
+    	//		System.out.println("FormFile="+formFile.getFileSize());
     			final IImportFormatService service = Injection.find(aRequest).getService(IImportFormatService.class);
-    			 final String originalFileName = formFile.getFileName(); 
+    			 final String originalFileName = form.getAttachmentFile().getFileName(); 
     		     final String tempFileName = FileUploadUtil.writeFile(formFile) ;
-    		     System.out.println("HELLOOOO "+tempFileName);
-    		     IRemoteMonitorService monitorService = (IRemoteMonitorService) Injection.find(aRequest).getService("MonitorService") ;
-    		     final long monitorId = monitorService.createMonitor();
+    	//	     System.out.println("HELLOOOO "+tempFileName);
+    		    
     		     final ImportFileForm timeForm = new ImportFileForm();
     		        timeForm.setActualDateFrom(form.getPeriod());
     		        timeForm.setActualDateTo(form.getPeriod());
     		        timeForm.setImportFormat(form.getImportFormat());
-    		        timeForm.setComment("AUTOIMPORT, date="+form.getPeriod());
-    		        new Thread() {
-    		           public synchronized void run() {
+    		        timeForm.setComment("AUTOIMPORT, Дата импорта = "+DateFormat.formatToDate(new java.util.Date()));
+    		        
+    		    //   new Thread() {
+    		    //       public synchronized void run() {
     		                try {
-    		                	service.setDebug(true);
-    		                    service.setUpdateModifiedOnly(true);
-    		                    service.setVerifyAfterSave(true);
-    		                
-    		                    service.importFile(monitorId, 0, tempFileName,originalFileName, timeForm) ;
-    		                   } catch (ImportException e) {
+    		                	service.importFile(monitorId,fileId , tempFileName, originalFileName, timeForm);
+    		                   } catch (Exception e) {
+    		                	   e.printStackTrace();
     		                    throw new IllegalStateException(e) ;
     		                }
-    		            }
+    		     //       }
 
-    		        }.start();
-    		        
-    		}
-    	
-    		 
-    		}
+    		    //    }.start();
+    		        aRequest.setAttribute("impResult", "Файл успешно проимпортирован!");
+    		} 
+    	}
         return aMapping.findForward("success") ;
     }
 }
