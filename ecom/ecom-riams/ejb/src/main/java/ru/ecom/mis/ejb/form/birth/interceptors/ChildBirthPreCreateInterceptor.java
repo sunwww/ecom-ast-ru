378914a3b11595ae1ca2ad8902a56dd71a1aedec
vasek00786 <vasek00786@users.noreply.github.com>
2016-07-08 15:15:05 +0400

    Добавлен запрет на создание нескольких случаев родов в отделении

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/form/birth/interceptors/ChildBirthPreCreateInterceptor.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/form/birth/interceptors/ChildBirthPreCreateInterceptor.java
@@ -15,6 +15,8 @@ public class ChildBirthPreCreateInterceptor implements IParentFormInterceptor {
 	    	ChildBirthForm form = (ChildBirthForm) aForm ; 
 	    	List<Object[]> list=aContext.getEntityManager().createNativeQuery("select slo.id,ml.id from medcase slo left join mislpu ml on ml.id=slo.department_id where  slo.id='"+form.getMedCase()+"' and ml.isMaternityWard='1'").getResultList() ;
 	    	if (list.size()==0) throw new IllegalStateException("Описание родов может быть заведена только в родильном отделении!!!") ;
+	    	list=aContext.getEntityManager().createNativeQuery("select cb.id from childbirth cb where  cb.medcase_id='"+form.getMedCase()+"'").getResultList() ;
+	    	if (list.size()>0) throw new IllegalStateException("В отделении уже оформлен случай родов!!") ;
     	}
     }
 }
