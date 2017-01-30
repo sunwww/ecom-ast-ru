ec9d88490f07cfa1561b65cdc8e5afe059d77312
Василий <vasek00786@users.noreply.github.com>
2017-01-30 09:02:23 +0400

    Парсинг pdf

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/diary/ejb/service/protocol/IKdlDiaryService.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/diary/ejb/service/protocol/IKdlDiaryService.java
@@ -1,7 +1,14 @@
 package ru.ecom.diary.ejb.service.protocol;
 
+import java.io.IOException;
+
+import org.json.JSONException;
+
+import com.itextpdf.text.DocumentException;
+
 public interface IKdlDiaryService {
 	public void parseFile(String aUri) throws Exception;
 	public String getDir(String aKey, String aDefaultDir) ;
 	public String run(String Command) ;
+	public void checkPdf() throws IOException, NoSuchFieldException, IllegalAccessException, JSONException ;
 }
