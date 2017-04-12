c55f97e5ceda481a9c8d0206dcb7b5a94aba03ce
Rashgild <Rashgild@gmail.com>
2017-04-12 14:28:12 +0400

    Close #6 add: Просмотр всех пациентов только по отделению (переход из ячейки "Всего") add: Просмотр пациентов по нужной диете и отделению (переход из перекрестной ячейки "Отделение" - "Диета")

--- ecom/ecom-riams/web/src/main/java/ru/ecom/mis/web/action/diet/DietJournalAction.java
+++ ecom/ecom-riams/web/src/main/java/ru/ecom/mis/web/action/diet/DietJournalAction.java
@@ -10,104 +10,126 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import ru.ecom.ejb.services.query.IWebQueryService;
-import ru.ecom.mis.web.action.medcase.PrescriptionForm;
 import ru.ecom.web.util.Injection;
-import ru.nuzmsh.commons.formpersistence.annotation.Comment;
 import ru.nuzmsh.web.struts.BaseAction;
 
 public class DietJournalAction extends BaseAction {
-	 public ActionForward myExecute(ActionMapping aMapping, ActionForm aForm, HttpServletRequest aRequest, HttpServletResponse aResponse) throws Exception {
-	    	DietJournalForm form = (DietJournalForm) aForm;
-	        form.validate(aMapping, aRequest) ;
-	        String dateFrom  = form.getBeginDate();
-	        Long diet = form.getDiet();
-	        if (dateFrom!=null&&!dateFrom.equals("")){
-	        	//String dateTo = form.getEndDate();
-	        //	if (dateTo ==null||dateTo.equals("")) {dateTo = dateFrom;}
-	        	
-		        IWebQueryService service = (IWebQueryService) Injection.find(aRequest).getService(IWebQueryService.class) ;
-		        StringBuilder sql = new StringBuilder();
-		    	StringBuilder sqlAdd = new StringBuilder();
-		    //	String leftJoinSql = "";
-		    	StringBuilder leftJoinAdd = new StringBuilder();
-		        String department = ""+form.getDepartment();
-		        String typeView = aRequest.getParameter("typeView");
-		        if (typeView!=null&&typeView.equals("2")) {
-		        	sqlAdd.append(" and p.planStartDate = to_date('"+dateFrom+"','dd.MM.yyyy') and p.planStartTime>=cast ('12:00:00' as time) ");
-		        }
-		    	if (department!=null&&!department.equals("")&&!department.equals("0")) {
-		    		sqlAdd.append(" and slo.department_id="+department);
-		    	}
-		    	if (diet!=null&&diet>0L) {
-		    		sqlAdd.append(" and p.diet_id="+diet);
-		    	}
-		    	
-		    	StringBuilder table = new StringBuilder();
-	    	
-	    	
-	    	    String tmpSql = "select d.id, d.name, count(d.id) as cntByDiet" +
-			    		" from prescription p " +
-			    		" left join diet d on d.id=p.diet_id" +
-			    		" left join prescriptionList pl on pl.id=p.prescriptionList_id" +
-		    			" left join medcase slo  on slo.id=pl.medcase_id" +
-			    		" where to_date('"+dateFrom+"','dd.MM.yyyy') between p.planStartDate and coalesce(p.planEndDate, current_date) and (p.planEndTime is null or p.planEndTime>=current_time) and p.dtype='DietPrescription' and p.diet_id is not null"+sqlAdd+ 
-			    		" group by d.id, d.name order by d.name";
-	    	    System.out.println("=== DietList = "+tmpSql);
-			    List<Object[]> dietList = service.executeNativeSqlGetObj(tmpSql);
-			    		
-			    if (dietList.size()>0) {
-			    	
-			    	StringBuilder selectAdd = new StringBuilder();
-			    	StringBuilder tableBottom = new StringBuilder();
-			    	
-			    	int k=3;
-			    	table.append("<table class='tabview cell' border='1'><tr><th>ИД</th><th>Отделение/Название диеты</th>");
-			    	tableBottom.append("<tr><td colspan='2'><b>ВСЕГО:</b></td>");
-			    	int sum = 0;
-			    	for (Object[] o: dietList) { 
-			    		table.append("<th colspan='1'>"+o[1]+"</th>");
-			    		tableBottom.append("<td>"+o[2]+"</td>");
-			    		//Формируем строку для основного запроса
-			    		selectAdd.append(", count(case when p.diet_id='"+o[0]+"' then p.id else null end) as cnt_"+k);			    		
-			    		k++;
-			    		sum +=Integer.parseInt(""+o[2]);
-			    		}
-			    	table.append("<th colspan='1'>ВСЕГО:</th>");
-			    	tableBottom.append("<td><b>").append(sum).append("</b></td>");
-			    	tableBottom.append("</tr>");
-			    	table.append("</tr>");	
-			    	//А вот и пришло время основного запроса
-			    	sql.append("select  dep.id as f1, dep.name as f2").append(selectAdd)
-			    		.append(", count(p.id) as cntAllByDepartment")
-			    		.append(" from prescription p"+
-			    			" left join prescriptionList pl on pl.id=p.prescriptionList_id" +
-			    			" left join medcase slo  on slo.id=pl.medcase_id" +
-			    			" left join mislpu dep on dep.id=slo.department_id")
-			    		.append(leftJoinAdd)
-			    		.append(" where p.dtype='DietPrescription' and to_date('"+dateFrom+"','dd.MM.yyyy') between p.planStartDate and coalesce(p.planEndDate, current_date) and (p.planEndTime is null or p.planEndTime>=current_time) ").append(sqlAdd)
-			    		.append("group by dep.id, dep.name order by dep.name");
-			    	System.out.println("=== SQL = "+sql);
-			    	List<Object[]> resultList = service.executeNativeSqlGetObj(sql.toString());
-			    	if (resultList.size()>0){
-			    		for (Object[] res: resultList) {
-			    			table.append("<tr>");
-			    			for (Object td: res) {
-			    				
-			    				table.append("<td>").append(""+td).append("</td>");
-			    			}
-			    			table.append("</tr>");
-			    		}
-			    		table.append(tableBottom);
-			    		table.append("</table>");
-			    aRequest.setAttribute("tableList", table.toString());	
-			  //  System.out.print("=== ResultText = "+table.toString());
-			    } else {
-			    	aRequest.setAttribute("tableList", "Данных не найдено");	
-			    }
-			    }
-			 }
-			    
-	        
-	        return aMapping.findForward("success");
+    public ActionForward myExecute(ActionMapping aMapping, ActionForm aForm,
+	    HttpServletRequest aRequest, HttpServletResponse aResponse)
+	    throws Exception {
+	DietJournalForm form = (DietJournalForm) aForm;
+	form.validate(aMapping, aRequest);
+	String dateFrom = form.getBeginDate();
+	Long diet = form.getDiet();
+	if (dateFrom != null && !dateFrom.equals("")) {
+
+	    IWebQueryService service = (IWebQueryService) Injection.find(
+		    aRequest).getService(IWebQueryService.class);
+	    StringBuilder sql = new StringBuilder();
+	    StringBuilder sqlAdd = new StringBuilder();
+	    StringBuilder leftJoinAdd = new StringBuilder();
+	    String department = "" + form.getDepartment();
+	    String typeView = aRequest.getParameter("typeView");
+	    if (typeView != null && typeView.equals("2")) {
+		sqlAdd.append(" and p.planStartDate = to_date('"+ dateFrom+ "','dd.MM.yyyy') and p.planStartTime>=cast ('12:00:00' as time) ");
 	    }
+	    if (department != null && !department.equals("")
+		    && !department.equals("0")) {
+		sqlAdd.append(" and slo.department_id=" + department);
+	    }
+	    if (diet != null && diet > 0L) {
+		sqlAdd.append(" and p.diet_id=" + diet);
+	    }
+
+	    StringBuilder table = new StringBuilder();
+
+	    String tmpSql = "select d.id, d.name, count(d.id) as cntByDiet"
+		    + " from prescription p "
+		    + " left join diet d on d.id=p.diet_id"
+		    + " left join prescriptionList pl on pl.id=p.prescriptionList_id"
+		    + " left join medcase slo  on slo.id=pl.medcase_id"
+		    + " where to_date('"+ dateFrom+ "','dd.MM.yyyy') between p.planStartDate and coalesce(p.planEndDate, current_date) "
+		    + "and (p.planEndTime is null or p.planEndTime>=current_time) and p.dtype='DietPrescription' and p.diet_id is not null"
+		    + sqlAdd + " group by d.id, d.name order by d.name";
+	    System.out.println("=== DietList = " + tmpSql);
+	    List<Object[]> dietList = service.executeNativeSqlGetObj(tmpSql);
+
+	    if (dietList.size() > 0) {
+
+		StringBuilder selectAdd = new StringBuilder();
+		StringBuilder tableBottom = new StringBuilder();
+
+		int k = 3;
+		table.append("<table class='tabview cell' border='1'><tr><th>ИД</th><th>Отделение/Название диеты</th>");
+		tableBottom.append("<tr><td colspan='2'><b>ВСЕГО:</b></td>");
+		int sum = 0;
+		StringBuilder sb = new StringBuilder();
+		for (Object[] o : dietList) {
+		    if (sb.length() > 0) {
+			sb.append("#");
+		    }
+		    sb.append("dietId=" + o[0]);
+		    table.append("<th colspan='1'>" + o[1] + "</th>");
+		    tableBottom.append("<td>" + o[2] + "</td>");
+		    // Формируем строку для основного запроса
+		    selectAdd.append(", count(case when p.diet_id='" + o[0]
+			    + "' then p.id else null end) as cnt_" + k);
+		    k++;
+		    sum += Integer.parseInt("" + o[2]);
+		}
+		String[] dietsLink = sb.toString().split("#");
+		table.append("<th colspan='1'>ВСЕГО:</th>");
+		tableBottom.append("<td><b>").append(sum).append("</b></td>");
+		tableBottom.append("</tr>");
+		table.append("</tr>");
+		// А вот и пришло время основного запроса
+		sql.append("select  dep.id as f1, dep.name as f2")
+			.append(selectAdd)
+			.append(", count(p.id) as cntAllByDepartment")
+			.append(" from prescription p")
+			.append(" left join prescriptionList pl on pl.id=p.prescriptionList_id")
+			.append(" left join medcase slo  on slo.id=pl.medcase_id")
+			.append(" left join mislpu dep on dep.id=slo.department_id ")
+			.append(leftJoinAdd)
+			.append(" where p.dtype='DietPrescription' and to_date('"+ dateFrom + "','dd.MM.yyyy')")
+			.append(" between p.planStartDate and coalesce(p.planEndDate, current_date) and (p.planEndTime is null or p.planEndTime>=current_time) ")
+			.append(sqlAdd)
+			.append("group by dep.id, dep.name order by dep.name");
+		// System.out.println("=== SQL = " + sql);
+		List<Object[]> resultList = service.executeNativeSqlGetObj(sql
+			.toString());
+		if (resultList.size() > 0) {
+		    for (Object[] res : resultList) {
+			table.append("<tr>");
+			int i = 0, fl = 0;
+
+			for (Object td : res) {
+			    table.append("<td");
+			    if (i > 1) {
+				table.append(" onclick=\"goToPage('diet_dietJournal.do?start="+ dateFrom + "&typeReestr=1")
+					.append("&depId=" + res[0])
+					.append("&")
+					.append(fl < dietsLink.length ? dietsLink[fl]:"").append("')\">");
+				fl++;
+			    } else {
+				table.append(">");
+			    }
+
+			    i++;
+			    table.append("" + td).append("</td>");
+			}
+
+			table.append("</tr>");
+		    }
+		    table.append(tableBottom);
+		    table.append("</table>");
+		    aRequest.setAttribute("tableList", table.toString());
+		    // System.out.print("=== ResultText = "+table.toString());
+		} else {
+		    aRequest.setAttribute("tableList", "Данных не найдено");
+		}
+	    }
+	}
+	return aMapping.findForward("success");
+    }
 }
--- ecom/ecom-riams/web/src/webapp/WEB-INF/actions/diet_diet/list_diet_prescriptions.jsp
+++ ecom/ecom-riams/web/src/webapp/WEB-INF/actions/diet_diet/list_diet_prescriptions.jsp
@@ -10,81 +10,111 @@
 <%@page import="ru.ecom.ejb.services.query.IWebQueryService"%>
 <%@page import="java.util.Collection"%>
 
-<style>
-h2 {display:none;}
-@media print {
-h2 {display:inline;}
-img,div#copyright,h1,ul#ideModeMainMenu, div#ideModeMainMenuClose {display:none;}
-input#beginDate{display:inline;}
-div.x-box-mc{display:none;}
-div#header{display:none;}
-}
-</style>
-<h2> Сводные  сведения по  наличию  больных, состоящих  на  питании <br></h2>
-<h2>на <%= request.getParameter("beginDate") %></h2>
 <tiles:insert page="/WEB-INF/tiles/mainLayout.jsp" flush="true" >
-
+	<tiles:put name="style" type="string">
+		<style>
+			h2 {display:none;}
+			@media print {
+			h2 {display:inline;}
+			img,div#copyright,h1,ul#ideModeMainMenu, div#ideModeMainMenuClose {display:none;}
+			input#beginDate{display:inline;}
+			div.x-box-mc{display:none;}
+			div#header{display:none;}
+			}
+		</style>
+	</tiles:put>
   <tiles:put name="title" type="string">
     <msh:title mainMenu="Diet" guid="65127a6f-d6d3-4b8e-b436-c6aeeaea35ae" title="Журнал диет" />
-   
   </tiles:put>
-  <tiles:put name="side" type="string">
-     <msh:sideMenu>
   
-        </msh:sideMenu>
-  </tiles:put>
-  <tiles:put name="body" type="string">
-  <%
-  String typeState =ActionUtil.updateParameter("PrescriptJournal","typeState","3", request) ;
-  String typeGroup =ActionUtil.updateParameter("PrescriptJournal","typeGroup","1", request) ;
-  String typeView =ActionUtil.updateParameter("PrescriptJournal","typeView","1", request) ;
-  String typeReestr =request.getParameter("typeReestr") ;
-  if (request.getParameter("short")==null ||request.getParameter("short").equals(""))  {
-	 %>
-	 
-  <msh:form action="/diet_dietJournal.do" defaultField="beginDate" disableFormDataConfirm="true" method="GET" guid="d7b31bc2-38f0-42cc-8d6d-19395273168f">
-    <msh:panel guid="6ae283c8-7035-450a-8eb4-6f0f7da8a8ff">
-      <msh:row guid="53627d05-8914-48a0-b2ec-792eba5b07d9">
-        <msh:separator label="Параметры поиска" colSpan="7" />
-      </msh:row>
-	 <msh:row>
-	        <td class="label" title="Отображать (typeView)" colspan="1"><label for="typeViewName" id="typeViewLabel">Отображать:</label></td>
-	        <td onclick="this.childNodes[1].checked='checked';">
-	        	<input type="radio" name="typeView" value="1">  Всех пациентов
-	        </td>
-	        
-	        <td onclick="this.childNodes[1].checked='checked';">
-	        	<input type="radio" name="typeView" value="2"> Поступивших после 12 часов
-	        </td>
-	       
-        </msh:row>
-        
-        <msh:row>
-        	<msh:autoComplete property="department" fieldColSpan="4" horizontalFill="true" label="Отделение" vocName="lpu" size="50"/>
-        </msh:row>
-        <msh:row>
-        	<msh:autoComplete property="diet"  fieldColSpan="4" horizontalFill="true" label="Диета" vocName="Diet"  size="50"/>
-        </msh:row>
-        
-      <msh:row>
-        <msh:textField property="beginDate" label="Период" />
-        
-           <td>
-            <input type="submit" value="Найти" />
-          </td>
-      </msh:row>
-    </msh:panel>
-    </msh:form>
-    <%} %>
-   
+  
+<tiles:put name="body" type="string">
+<h2> Сводные  сведения по  наличию  больных, состоящих  на  питании <br></h2>
+<h2>на <%=request.getParameter("beginDate")%></h2>
+  
+ <%
+	String typeState = ActionUtil.updateParameter("PrescriptJournal", "typeState", "3", request);
+	String typeGroup = ActionUtil.updateParameter("PrescriptJournal", "typeGroup", "1", request);
+	String typeView = ActionUtil.updateParameter("PrescriptJournal", "typeView", "1", request);
+	String typeReestr = request.getParameter("typeReestr");
+	
+	String start = request.getParameter("start");
+	request.setAttribute("start", start);
+	String depId = request.getParameter("depId");
+	request.setAttribute("depId", depId);
+	
+	String dietId = request.getParameter("dietId");
+	request.setAttribute("dietId", dietId);
+	
+	if (request.getParameter("short") == null || request.getParameter("short").equals("")) { %>
+	
+	<msh:form action="/diet_dietJournal.do" defaultField="beginDate" disableFormDataConfirm="true" method="GET" guid="d7b31bc2-38f0-42cc-8d6d-19395273168f">
+		<msh:panel guid="6ae283c8-7035-450a-8eb4-6f0f7da8a8ff">
+			<msh:row guid="53627d05-8914-48a0-b2ec-792eba5b07d9">
+				<msh:separator label="Параметры поиска" colSpan="7" />
+			</msh:row>
+			<msh:row>
+				<td class="label" title="Отображать (typeView)" colspan="1">
+				<label for="typeViewName" id="typeViewLabel">Отображать:</label></td>
+				<td onclick="this.childNodes[1].checked='checked';">
+				<input type="radio" name="typeView" value="1"> Всех пациентов</td>
+				<td onclick="this.childNodes[1].checked='checked';">
+				<input type="radio" name="typeView" value="2"> Поступивших после12 часов</td>
+	
+			</msh:row>
+			<msh:row>
+				<msh:autoComplete property="department" fieldColSpan="4" horizontalFill="true" label="Отделение" vocName="lpu" size="50" />
+			</msh:row>
+			<msh:row>
+				<msh:autoComplete property="diet" fieldColSpan="4" horizontalFill="true" label="Диета" vocName="Diet" size="50" />
+			</msh:row>
+			<msh:row>
+				<msh:textField property="beginDate" label="Период" />
+				<td><input type="submit" value="Найти" /></td>
+			</msh:row>
+		</msh:panel>
+	</msh:form>
+		<%
+		}
+  if (typeReestr != null && (typeReestr.equals("1"))) {
+      String SQL="";
+      if(dietId != null){
+	  SQL = " and diet_id="+dietId;
+      }
+      request.setAttribute("SQL", SQL);
+		%>
+	<msh:section>
+		<msh:sectionContent>
+			<ecom:webQuery maxResult="1500" name="journal_ticket" nativeSql="SELECT  pat.id ,pat.lastname||' '||pat.firstname||' '||pat.middlename as fio, dep.name, d.name  as diet,p.planstartdate,p.planEndDate
+			from prescription p
+			left join prescriptionList pl on pl.id=p.prescriptionList_id 
+			left join diet d on d.id=p.diet_id
+			left join medcase slo  on slo.id=pl.medcase_id 
+			left join mislpu dep on dep.id=slo.department_id 
+			left join patient pat on pat.id = slo.patient_id
+			where  
+			p.dtype='DietPrescription' 
+			and to_date('${start}','dd.MM.yyyy') 
+			between p.planStartDate and coalesce(p.planEndDate, current_date) and dep.id=${depId} ${SQL}
+			and (p.planEndTime is null or p.planEndTime>=current_time)
+			order by d.name" /> 
+			<msh:table name="journal_ticket" action="entityView-mis_patient.do" idField="1" noDataMessage="Нет данных">
+				<msh:tableColumn columnName="#" property="sn" />
+				<msh:tableColumn columnName="ФИО пациента" property="2" />            
+				<msh:tableColumn columnName="Отделение" property="3"/>
+				<msh:tableColumn columnName="Диета" property="4"/>
+			</msh:table>
+		</msh:sectionContent>
+	</msh:section>
     <%
-    String dateFrom = request.getParameter("beginDate");
-    if (dateFrom!=null&&!dateFrom.equals("")) {
-    	out.print(request.getAttribute("tableList"));
-     } %>
+        }
+		String dateFrom = request.getParameter("beginDate");
+  		if (dateFrom != null && !dateFrom.equals("")) { out.print(request.getAttribute("tableList"));}
+    %>
     <script type="text/javascript" src="./dwr/interface/PrescriptionService.js"></script>
     <script type="text/javascript" src="./dwr/interface/HospitalMedCaseService.js"></script>
     <script type='text/javascript'>
+    
     function checkFieldUpdate(aField,aValue,aDefault) {
     	eval('var chk =  document.forms[0].'+aField) ;
     	var max = chk.length ;
@@ -94,16 +124,9 @@ div#header{display:none;}
     		chk[+aValue-1].checked='checked' ;
     	}
     }
-           checkFieldUpdate('typeView','${typeView}',1) ;
-         //  checkFieldUpdate('typeGroup','${typeGroup}',1) ;
-//           checkFieldUpdate('typeState','${typeState}',1) ;
-        //   checkFieldUpdate('typeTransfer','${typeTransfer}',1) ;
-       //    checkFieldUpdate('typeService','${typeService}',1) ;
-
+	checkFieldUpdate('typeView','${typeView}',1);
   </script>
-  <br>
-  
-  <h2>Составила: медицинская  сестра  диетическая___________________ Шеф-повар______________ </h2>
+  <br><h2>Составила: медицинская  сестра  диетическая___________________ Шеф-повар______________ </h2>
   </tiles:put>
 </tiles:insert>
 
