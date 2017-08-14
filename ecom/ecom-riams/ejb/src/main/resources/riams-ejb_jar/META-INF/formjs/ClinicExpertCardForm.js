9077601ef89dac6fb72c2cc7f8f6cf069b015381
vtsybulin <vasek007@mail.ru>
2017-08-14 13:41:15 +0400

    Исправлено присвоение номера при создании ВК (без создания направления на ВК)

--- ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/ClinicExpertCardForm.js
+++ ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/ClinicExpertCardForm.js
@@ -6,22 +6,7 @@ function onSave(aForm, aEntity, aCtx) {
 	aEntity.setEditDate(new java.sql.Date(date.getTime())) ;
 	aEntity.setEditTime(new java.sql.Time (date.getTime())) ;
 	aEntity.setEditUsername(aCtx.getSessionContext().getCallerPrincipal().toString()) ;
-	if (aEntity.getNumberInJournal()==null||aEntity.getNumberInJournal()=="") {
-		if (aEntity.getExpertDate!=null) {
-			//Проставляем порядковый номер в журнале. номер уникальный в пределах типа ВК и года-месяца экспертизы
-			var code = aEntity.getType().getCode();
-            var cal = java.util.Calendar.getInstance() ;
-            var format = new java.text.SimpleDateFormat("yyyy-MM") ;
-            cal.setTime(aEntity.getExpertDate());
-			code="clinicexpert#"+code+"#"+ format.format(cal.getTime());
-			var helper = Packages.ru.ecom.ejb.sequence.service.SequenceHelper.getInstance();
-			var num = helper.startUseNextValueNoCheck(code,"",aCtx.manager);
-			aEntity.setNumberInJournal(""+num);
-			aCtx.manager.persist(aEntity);
-
-		}
-	}
-
+    setJournalNumber(aEntity, aCtx);
 }
 /**
  * При создании
@@ -31,5 +16,20 @@ function onCreate(aForm, aEntity, aCtx) {
 	aEntity.setCreateDate(new java.sql.Date(date.getTime())) ;
 	aEntity.setCreateTime(new java.sql.Time (date.getTime())) ;
 	aEntity.setCreateUsername(aCtx.getSessionContext().getCallerPrincipal().toString()) ;
+    setJournalNumber(aEntity, aCtx);
+}
 
+//Проставляем порядковый номер в журнале. номер уникальный в пределах типа ВК и года-месяца экспертизы
+function setJournalNumber(aEntity, aCtx) {
+    if ((aEntity.getNumberInJournal()==null||aEntity.getNumberInJournal()=="") && aEntity.getExpertDate!=null) {
+    	var code = aEntity.getType().getCode();
+        var cal = java.util.Calendar.getInstance() ;
+        var format = new java.text.SimpleDateFormat("yyyy-MM") ;
+        cal.setTime(aEntity.getExpertDate());
+        code="clinicexpert#"+code+"#"+ format.format(cal.getTime());
+        var helper = Packages.ru.ecom.ejb.sequence.service.SequenceHelper.getInstance();
+        var num = helper.startUseNextValueNoCheck(code,"",aCtx.manager);
+        aEntity.setNumberInJournal(""+num);
+        aCtx.manager.persist(aEntity);
+    }
 }
