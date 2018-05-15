9351da5c6672eb7e11f30bdcc8bcedf378458bb4
vtsybulin <vasek007@mail.ru>
2018-05-15 14:37:24 +0400

    Запрет на редактирование записи если заполнение закрыто

--- ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/E2EntryForm.js
+++ ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/E2EntryForm.js
@@ -30,6 +30,9 @@ function onSave(aForm, aEntity, aContext) {
  * Перед сохранением
  */
 function onPreSave(aForm, aEntity, aContext) {
+    if (!aContext.manager.createNativeQuery("select list.id from e2listentry list where list.id="+aEntity.getListEntry().getId()+" and list.isClosed='1'").getResultList().isEmpty()) {
+        throw "Заполнение закрыто, редактирование записи невозможно!";
+    }
 }
 
 /**
