5b07733c16f33d07510728e16b2e4ccacd07b1e1
vtsybulin <vasek007@mail.ru>
2018-05-18 15:06:26 +0400

    Экспертиза: статусы счетов, начата работа по выгрузке ДД

---
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/expert2/domain/E2Bill.java
@@ -0,0 +1,44 @@
+package ru.ecom.expert2.domain;
+
+import ru.ecom.ejb.domain.simple.BaseEntity;
+import ru.ecom.expert2.domain.voc.VocE2BillStatus;
+import ru.ecom.expomc.ejb.domain.registry.RegInsuranceCompany;
+import ru.nuzmsh.commons.formpersistence.annotation.Comment;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import java.sql.Date;
+
+@Entity
+public class E2Bill extends BaseEntity {
+
+    /** Номер счета */
+    @Comment("Номер счета")
+    public String getBillNumber() {return theBillNumber;}
+    public void setBillNumber(String aBillNumber) {theBillNumber = aBillNumber;}
+    /** Номер счета */
+    private String theBillNumber ;
+
+    /** Дата счета */
+    @Comment("Дата счета")
+    public Date getBillDate() {return theBillDate;}
+    public void setBillDate(Date aBillDate) {theBillDate = aBillDate;}
+    /** Дата счета */
+    private Date theBillDate ;
+
+    /** Статус счета */
+    @Comment("Статус счета")
+    @OneToOne
+    public VocE2BillStatus getStatus() {return theStatus;}
+    public void setStatus(VocE2BillStatus aStatus) {theStatus = aStatus;}
+    /** Статус счета */
+    private VocE2BillStatus theStatus ;
+
+    /** Страховая компания */
+    @Comment("Страховая компания")
+    @OneToOne
+    public RegInsuranceCompany getCompany() {return theCompany;}
+    public void setCompany(RegInsuranceCompany aCompany) {theCompany = aCompany;}
+    /** Страховая компания */
+    private RegInsuranceCompany theCompany ;
+}
