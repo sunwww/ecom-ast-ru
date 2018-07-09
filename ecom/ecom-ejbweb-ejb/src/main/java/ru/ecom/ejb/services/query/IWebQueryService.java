b8bf1e04f5b96e6605453e8c5fd107a69e909ba9
Rashgild <Rashgild@gmail.com>
2018-07-09 11:32:06 +0400

    #56 ЭЛН #api add: возврат из sql-запроса - JSON

--- ecom/ecom-ejbweb-ejb/src/main/java/ru/ecom/ejb/services/query/IWebQueryService.java
+++ ecom/ecom-ejbweb-ejb/src/main/java/ru/ecom/ejb/services/query/IWebQueryService.java
@@ -1,16 +1,29 @@
 package ru.ecom.ejb.services.query;
 
+
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.naming.NamingException;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 public interface IWebQueryService {
+
+	String executeSqlGetJson(String aQuery,Integer limit,String nameArray) throws NamingException, SQLException;
+	String executeSqlGetJson(String aQuery,Integer limit) throws NamingException, SQLException;
+	//JSONArray executeSqlGetJsonArray(String aQuery) throws SQLException, NamingException;
+	String executeSqlGetJsonObject(String aQuery) throws NamingException, SQLException;
 	String executeNativeSqlGetJSON(String[] aFieldNames, String aQuery, Integer aMaxResult);
 	Collection<WebQueryResult> executeHql(String aQuery) ;
 	Collection<WebQueryResult> executeNativeSql(String aQuery) ;
 	Collection<WebQueryResult> executeHql(String aQuery,Integer aMaxResult) ;
 	Collection<WebQueryResult> executeNativeSql(String aQuery,Integer aMaxResult) ;
 	
-	public int executeUpdateNativeSql(String aQuery) ;
+	int executeUpdateNativeSql(String aQuery) ;
 	
 	List<Object[]> executeNativeSqlGetObj(String aQuery,Integer aMaxResult) ;
 	List<Object[]> executeNativeSqlGetObj(String aQuery) ;
