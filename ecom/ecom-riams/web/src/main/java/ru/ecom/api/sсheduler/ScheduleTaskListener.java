0ec663a1debab2a97167e9976e299cade917d19e
Rashgild <Rashgild@gmail.com>
2018-09-13 11:58:39 +0400

    #116 Планировщик -add: запуск только на конкретном сервере.

--- ecom/ecom-riams/web/src/main/java/ru/ecom/api/sсheduler/ScheduleTaskListener.java
+++ ecom/ecom-riams/web/src/main/java/ru/ecom/api/sсheduler/ScheduleTaskListener.java
@@ -3,15 +3,18 @@ package ru.ecom.api.sсheduler;
 import org.json.JSONException;
 import ru.ecom.ejb.services.query.IWebQueryService;
 import ru.ecom.ejb.services.query.WebQueryResult;
+import ru.ecom.mis.ejb.service.disability.IDisabilityService;
 import ru.ecom.web.util.Injection;
 
+import javax.management.*;
 import javax.naming.NamingException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
 
 /** Created by rkurbanov on 10.09.2018. */
@@ -25,14 +28,21 @@ public class ScheduleTaskListener  implements ServletContextListener {
         System.out.println("ScheduleTaskListener is start");
 
         try {
-            IWebQueryService service = Injection.find(event, "riams", "66405d38-a173-4cb7-a1b6-3ada51c16ac5").getService(IWebQueryService.class);
-            Collection<WebQueryResult> list = service.executeNativeSql("select id,name,link, time from ScheduleTask");
+            IDisabilityService service1 = Injection.find(event,"riams", "66405d38-a173-4cb7-a1b6-3ada51c16ac5")
+                    .getService(IDisabilityService.class);
+            String endpoint = service1.getSoftConfigValue("EndpointApi", "null");
 
-            ScheduleTasks scheduleTasks  = new ScheduleTasks();
-
-            for (WebQueryResult wqr : list){
-                Long id = Long.valueOf(wqr.get1().toString());
-                map = scheduleTasks.startThread(id,wqr.get2().toString(),wqr.get3().toString(),wqr.get4().toString());
+            endpoint = "http://"+endpoint.split("/")[2];
+            if(getEndPoints().contains(endpoint)) {
+                System.out.println("This is TRUE server");
+                IWebQueryService service = Injection.find(event, "riams", "66405d38-a173-4cb7-a1b6-3ada51c16ac5")
+                        .getService(IWebQueryService.class);
+                Collection<WebQueryResult> list = service.executeNativeSql("select id,name,link, time from ScheduleTask");
+                ScheduleTasks scheduleTasks = new ScheduleTasks();
+                for (WebQueryResult wqr : list) {
+                    Long id = Long.valueOf(wqr.get1().toString());
+                    map = scheduleTasks.startThread(id, wqr.get2().toString(), wqr.get3().toString(), wqr.get4().toString());
+                }
             }
 
         }catch (NamingException e) {
@@ -48,6 +58,30 @@ public class ScheduleTaskListener  implements ServletContextListener {
                 ScheduledExecutorService scheduledExecutorService = (ScheduledExecutorService)entry.getKey();
                 scheduledExecutorService.shutdownNow();
                 scheduledExecutorService.shutdown();
+        }
+    }
+
+    private List<String> getEndPoints() {
+        try {
+            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+            Set<ObjectName> objs = mbs.queryNames( new ObjectName( "*:type=Connector,*" ),
+                    Query.match( Query.attr( "protocol" ), Query.value( "HTTP/1.1" ) ) );
+            String hostname = InetAddress.getLocalHost().getHostName();
+            InetAddress[] addresses = InetAddress.getAllByName( hostname );
+            ArrayList<String> endPoints = new ArrayList<>();
+            for ( ObjectName obj : objs ) {
+                String scheme = mbs.getAttribute( obj, "scheme" ).toString();
+                String port = obj.getKeyProperty( "port" );
+                for ( InetAddress addr : addresses ) {
+                    String host = addr.getHostAddress();
+                    String ep = scheme + "://" + host + ":" + port;
+                    endPoints.add( ep );
+                }
+            }
+            return endPoints;
+        } catch ( MalformedObjectNameException | UnknownHostException | MBeanException | AttributeNotFoundException | InstanceNotFoundException | ReflectionException ex ) {
+            ex.printStackTrace();
+            return new ArrayList<>();
         }
     }
 }
