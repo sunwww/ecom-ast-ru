df99b2b509dca50325a7f1d935a8f3dcc14ac160
vtsybulin <vasek007@mail.ru>
2018-12-04 16:17:56 +0400

    Зачатки ДД в экспертизе

---
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/expert2/domain/E2ExtDispMedServiceLink.java
@@ -0,0 +1,11 @@
+package ru.ecom.expert2.domain;
+
+import ru.ecom.ejb.domain.simple.BaseEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+/**Таблица соответствия услуги в доп. диспансеризации и услуги по справочнику V001*/
+public class E2ExtDispMedServiceLink extends BaseEntity {
+    //hello world mazakafaafsj
+}
