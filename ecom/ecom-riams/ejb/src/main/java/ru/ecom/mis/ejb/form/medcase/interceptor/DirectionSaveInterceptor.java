44e4bc83c5829c2019c8ea3d5fa63e38cebfd3bd
vtsybulin <vasek007@mail.ru>
2019-02-05 15:11:51 +0400

    Убран неиспользуемый функционал

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/form/medcase/interceptor/DirectionSaveInterceptor.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/form/medcase/interceptor/DirectionSaveInterceptor.java
@@ -1,41 +1,11 @@
 package ru.ecom.mis.ejb.form.medcase.interceptor;
 
-import ru.ecom.diary.ejb.service.template.TemplateProtocolServiceBean;
 import ru.ecom.ejb.services.entityform.IEntityForm;
 import ru.ecom.ejb.services.entityform.interceptors.IFormInterceptor;
 import ru.ecom.ejb.services.entityform.interceptors.InterceptorContext;
-import ru.ecom.mis.ejb.domain.medcase.Visit;
-import ru.ecom.mis.ejb.form.medcase.poly.DirectionMedCaseForm;
-
-import javax.persistence.EntityManager;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 public class DirectionSaveInterceptor implements IFormInterceptor {
 
     public void intercept(IEntityForm aForm, Object aEntity, InterceptorContext aContext) {
-    	EntityManager manager = aContext.getEntityManager();
-    	DirectionMedCaseForm form = (DirectionMedCaseForm)aForm ;
-		List<Object[]> rec = manager.createNativeQuery("select pat.lastname||' '||substring(pat.firstname,1,1)||''||substring(coalesce(pat.middlename,' '),1,1) as f1_name , pr.phonenumber " +
-				" from patientlistrecord pr left join patient pat on pat.id=pr.patient where pr.patient="+form.getPatient()+" and pr.phoneNumber is not null").getResultList();
-		if (rec.size()>0) {
-			Object[] r = rec.get(0);
-			try {
-				Visit vis = (Visit) aEntity;
-					SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-					String wfName  =vis.getWorkFunctionPlan()!=null?(vis.getWorkFunctionPlan().getWorkFunction()!=null?vis.getWorkFunctionPlan().getWorkFunction().getName():""):"неопределен";
-					String date =  format.format(vis.getDatePlan().getCalendarDate());
-					String time = vis.getTimePlan().getTimeFrom().toString();
-					String message = "Пациент "+r[0].toString()+" записан к "+wfName+" на "+time+" "+date;
-					String phone = r[1].toString();
-
-					TemplateProtocolServiceBean bean = new TemplateProtocolServiceBean();
-					bean.sendSms(phone,message);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	
     }
 }
