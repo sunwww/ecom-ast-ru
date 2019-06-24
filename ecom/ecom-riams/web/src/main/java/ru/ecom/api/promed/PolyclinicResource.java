6009db4bbd20b01737ed14bd3a36ad4e6250707f
Milamesher <milamesher@yandex.ru>
2019-06-24 12:46:23 +0400

    #162 Написан метод GET getWfInfo, который принимает workfunction_id и из workfunction возвращает ФИО, отделение и promedcode_lpusection и promedcode_workstaff      Написан метод POST setWfInfo, который принимает workfunction_id (обязательный параметр) и      promedcode_lpusection (nullable) и promedcode_workstaff (nullable) и обновляет в таблице workfunction данную рабочую функцию.      В методе getPolyclinicCase в json в блоке WorkStaffInfo добавлен workFunctionId      В методе getPolyclinicCase исключены визиты, которые имеют некорректный dateTime ( вместо "yyyy-MM-dd HH:mm:ss" содержится "yyyy-MM-dd_"), т.е. нет времени исполнения (timeExecute)      Добавлены проверки на пустые параметры

--- ecom/ecom-riams/web/src/main/java/ru/ecom/api/promed/PolyclinicResource.java
+++ ecom/ecom-riams/web/src/main/java/ru/ecom/api/promed/PolyclinicResource.java
@@ -1,6 +1,7 @@
 package ru.ecom.api.promed;
 
 
+import org.json.JSONObject;
 import ru.ecom.api.util.ApiUtil;
 import ru.ecom.web.util.Injection;
 
@@ -32,14 +33,25 @@ public class PolyclinicResource {
             , @QueryParam("dateTo") String dateTo, @QueryParam("sstream") String sstream
             , @QueryParam("isUpload") Boolean isUpload
             , @QueryParam("includeNeoUzi") Boolean includeNeoUzi
-    ) throws NamingException, ParseException {
+    ) throws NamingException {
         if (aToken!=null) {ApiUtil.login(aToken,aRequest);}
         ApiUtil.init(aRequest,aToken);
         if (isUpload==null) isUpload=false;
         IApiPolyclinicService service =Injection.find(aRequest).getService(IApiPolyclinicService.class);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat format;
+        java.sql.Date d;
+        try {
+            format = new SimpleDateFormat("yyyy-MM-dd");
+            d = new java.sql.Date(format.parse(dateTo).getTime());
+        }
+        catch(NullPointerException | ParseException ex) {
+            JSONObject res = new JSONObject();
+            res.put("status","error")
+                    .put("reason","incorrect dateTo");
+            return res.toString();
+        }
         if (sstream==null || sstream.equals("")) sstream="OBLIGATORYINSURANCE";
-        return service.getPolyclinicCase(new java.sql.Date(format.parse(dateTo).getTime()),sstream,isUpload, includeNeoUzi!=null && includeNeoUzi);
+        return service.getPolyclinicCase(d,sstream,isUpload, includeNeoUzi!=null && includeNeoUzi);
     }
 
     @POST
@@ -61,5 +73,49 @@ public class PolyclinicResource {
         ApiUtil.init(aRequest,aToken);
         IApiPolyclinicService service =Injection.find(aRequest).getService(IApiPolyclinicService.class);
         return service.setEvnTap(medcase_id,tap_id);
+    }
+
+    @GET
+    @Path("getWfInfo")
+    @Produces(MediaType.APPLICATION_JSON)
+    /**
+     * Получить ФИО, отделение и promedcode_lpusection и promedcode_workstaff.
+     *
+     * @param aRequest HttpServletRequest
+     * @param aToken String
+     * @param workfunction_id Long Рабочая функция
+     * @return JSON in String
+     */
+    public String getWfInfo(@Context HttpServletRequest aRequest, @WebParam(name="token") String aToken
+            , @QueryParam("workfunction_id") Long workfunction_id
+    ) throws NamingException, ParseException {
+        if (aToken!=null) {ApiUtil.login(aToken,aRequest);}
+        ApiUtil.init(aRequest,aToken);
+        IApiPolyclinicService service =Injection.find(aRequest).getService(IApiPolyclinicService.class);
+        return service.getWfInfo(workfunction_id);
+    }
+
+
+    @POST
+    @Path("setWfInfo")
+    @Produces(MediaType.APPLICATION_JSON)
+    /**
+     * Установить promedcode_lpusection и promedcode_workstaff.
+     *
+     * @param aRequest HttpServletRequest
+     * @param aToken String
+     * @param workfunction_id Long Рабочая функция
+     * @param promedcode_lpusection String promedcode_lpusection
+     * @param promedcode_workstaff String promedcode_workstaff
+     * @return JSON in String
+     */
+    public String setWfInfo(@Context HttpServletRequest aRequest, @WebParam(name="token") String aToken
+            , @QueryParam("workfunction_id") Long workfunction_id
+            , @QueryParam("promedcode_lpusection") String promedcode_lpusection, @QueryParam("promedcode_workstaff") String promedcode_workstaff
+    ) throws NamingException {
+        if (aToken!=null) {ApiUtil.login(aToken,aRequest);}
+        ApiUtil.init(aRequest,aToken);
+        IApiPolyclinicService service =Injection.find(aRequest).getService(IApiPolyclinicService.class);
+        return service.setWfInfo(workfunction_id,promedcode_lpusection,promedcode_workstaff);
     }
 }
