72f220425dc2739f3be7f289e64c3f0037c5da26
Milamesher <milamesher@yandex.ru>
2019-11-21 15:41:47 +0400

    #181 fiz query

--- ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/WorkCalendarHospitalBedByHospForm.js
+++ ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/WorkCalendarHospitalBedByHospForm.js
@@ -47,7 +47,7 @@ function onPreCreate(aForm, aCtx) {
 }
 
 function checkDouble(aPatientId, aDateHosp, aId, aCtx) {
-	var sql = "select pre.id from WorkCalendarHospitalBed pre where pre.dtype='WorkCalendarHospitalBed' pre.patient_id="+aPatientId+" and pre.dateFrom = to_date('"+aDateHosp+"','dd.MM.yyyy')" ;
+	var sql = "select pre.id from WorkCalendarHospitalBed pre where pre.dtype='WorkCalendarHospitalBed' and pre.patient_id="+aPatientId+" and pre.dateFrom = to_date('"+aDateHosp+"','dd.MM.yyyy')" ;
 	if (+aId>0) sql +=" and pre.id!="+aId;
 	if (!aCtx.manager.createNativeQuery(sql).getResultList().isEmpty()) {
 		throw "У пациента уже создана предварительная госпитализация на это число, создание еще одной невозможно!";
