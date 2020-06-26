dd6a68853e6fbbc7b048a185bd3fba907fc07c2b
vtsybulin <vasek007@mail.ru>
2020-06-26 13:50:55 +0400

    fix

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/form/medcase/report/ReportParameterVarietyForm.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/form/medcase/report/ReportParameterVarietyForm.java
@@ -14,36 +14,38 @@ import ru.nuzmsh.ejb.formpersistence.annotation.EntityFormPersistance;
 @EntityFormSecurityPrefix("/Policy/Voc/ReportConfig")
 public class ReportParameterVarietyForm  extends IdEntityForm {
 
+	/** Тип параметра */
+	@Comment("Тип параметра")
+	@Persist
+	public Long getParameterType() {return theParameterType;}
+	public void setParameterType(Long aParameterType) {theParameterType = aParameterType;}
+	private Long theParameterType;
+
 	/** Начальный параметр */
 	@Comment("Начальный параметр")
 	@Persist
 	public String getCodeFrom() {return theCodeFrom;}
 	public void setCodeFrom(String aCodeFrom) {theCodeFrom = aCodeFrom;}
+	private String theCodeFrom;
 
 	/** Последний параметер */
 	@Comment("Последний параметр")
 	@Persist
 	public String getCodeTo() {return theCodeTo;}
 	public void setCodeTo(String aCodeTo) {theCodeTo = aCodeTo;}
+	private String theCodeTo;
 
 	/** Пол код */
 	@Comment("Пол код")
 	@Persist
 	public String getSexCode() {return theSexCode;}
 	public void setSexCode(String aSexCode) {theSexCode = aSexCode;}
+	private String theSexCode;
 
 	/** Пол */
 	@Comment("Пол")
 	@Persist
 	public Long getSex() {return theSex;}
 	public void setSex(Long aSex) {theSex = aSex;}
-
-	/** Пол */
 	private Long theSex;
-	/** Пол код */
-	private String theSexCode;
-	/** Последний параметр */
-	private String theCodeTo;
-	/** Начальный параметр */
-	private String theCodeFrom;
 }
---
+++ ecom/ecom-riams/web/src/main/java/ru/ecom/mis/web/action/kdl/CovidAnalysisImportAction.java
@@ -0,0 +1,60 @@
+package ru.ecom.mis.web.action.kdl;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import ru.ecom.diary.ejb.service.protocol.IKdlDiaryService;
+import ru.ecom.expert2.service.IExpert2ImportService;
+import ru.ecom.expert2.web.form.ImportFileForm;
+import ru.ecom.web.login.LoginInfo;
+import ru.ecom.web.util.Injection;
+import ru.nuzmsh.web.struts.BaseAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CovidAnalysisImportAction extends BaseAction {
+
+    public static final Logger LOG = Logger.getLogger(BaseAction.class);
+
+    @Override
+    public ActionForward myExecute(ActionMapping aMapping, ActionForm aForm,
+                                   HttpServletRequest aRequest, HttpServletResponse aResponse)
+            throws Exception {
+          ImportFileForm form = (ImportFileForm)aForm;
+      if (form.getFile()!=null && form.getFile().getFileSize()>0) {
+            IKdlDiaryService service = Injection.find(aRequest).getService(IKdlDiaryService.class);
+            IExpert2ImportService expert2service = Injection.find(aRequest).getService(IExpert2ImportService.class);
+            String username = LoginInfo.find(aRequest.getSession(true)).getUsername() ;
+            String uploadDir = expert2service.getConfigValue("tomcat.data.dir","/opt/tomcat/webapps/rtf");
+            String filename =uploadDir+"/"+ System.currentTimeMillis()+"#"+form.getFile().getFileName();
+            saveFile(form.getFile().getInputStream(),filename);
+            service.importCovidAnalysis(filename, username);
+        }
+
+        return aMapping.findForward(SUCCESS);
+    }
+
+    private void saveFile(InputStream aInputStream, String aFileName) throws IOException {
+        File outputFile = new File(aFileName);
+        if (outputFile.exists() || outputFile.createNewFile()) {
+            int count;
+            try (FileOutputStream out = new FileOutputStream(aFileName)) {
+                byte[] buf = new byte[8192];
+                while ((count = aInputStream.read(buf)) > 0) {
+                    out.write(buf, 0, count);
+                }
+                aInputStream.close();
+            } catch (Exception e) {
+                LOG.error("Невозможно записать файл: "+e.getMessage(),e);
+            }
+        } else {
+            LOG.error("Невозможно открыть / создать файл: "+aFileName);
+        }
+    }
+}
