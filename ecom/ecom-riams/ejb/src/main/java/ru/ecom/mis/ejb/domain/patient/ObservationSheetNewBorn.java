9bdf5e5141ad4a3018fca31b7b04f4187774723a
vasek00786 <vasek007@gmail.com>
2021-01-27 20:11:02 +0300

    drop неверный индексы

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/domain/patient/ObservationSheetNewBorn.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/mis/ejb/domain/patient/ObservationSheetNewBorn.java
@@ -1,18 +1,8 @@
 package ru.ecom.mis.ejb.domain.patient;
 
-
-import ru.ecom.ejb.services.index.annotation.AIndex;
-import ru.ecom.ejb.services.index.annotation.AIndexes;
-
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 /** Лист наблюдения новорождённого*/
 @Entity
-@Table(schema="SQLUser")
-@AIndexes({
-        @AIndex(properties="patient"),
-        @AIndex(properties="startDate")
-})
 public class ObservationSheetNewBorn extends ObservationSheet {
 }
