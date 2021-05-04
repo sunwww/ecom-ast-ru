073e4163b8f15e99ab209c895293c7ad3b99f83e
vasek00786 <vasek007@gmail.com>
2021-05-04 09:09:28 +0300

    fix vb_p

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/expert2/service/Expert2XmlServiceBean.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/expert2/service/Expert2XmlServiceBean.java
@@ -47,8 +47,7 @@ import static ru.nuzmsh.util.BooleanUtils.isNotTrue;
 import static ru.nuzmsh.util.BooleanUtils.isTrue;
 import static ru.nuzmsh.util.CollectionUtil.isEmpty;
 import static ru.nuzmsh.util.CollectionUtil.isNotEmpty;
-import static ru.nuzmsh.util.EqualsUtil.isAnyIsNull;
-import static ru.nuzmsh.util.EqualsUtil.isOneOf;
+import static ru.nuzmsh.util.EqualsUtil.*;
 import static ru.nuzmsh.util.StringUtil.isNullOrEmpty;
 
 @Stateless
@@ -191,7 +190,7 @@ public class Expert2XmlServiceBean implements IExpert2XmlService {
     /**
      * Создаем тэг с информацией о госпитализации (версия 3.2*)
      */
-    private Element createZSl2020(E2Entry entry, boolean isPoliclinic, int slCnt, int zslIdCase, String lpuRegNumber
+    private Element createZSl2020(E2Entry entry, boolean isPoliclinic, int zslIdCase, String lpuRegNumber
             , boolean a3) {
         String forPom = isTrue(entry.getIsEmergency()) ? (isPoliclinic ? "2" : "1") : "3";
         Element z = new Element("Z_SL");
@@ -221,10 +220,11 @@ public class Expert2XmlServiceBean implements IExpert2XmlService {
             add(z, "ISHOD", entry.getFondIshod().getCode()); // Исход случая.
         }
         addIfNotNull(z, "OS_SLUCH", Expert2FondUtil.calculateFondOsSluch(entry)); // Особый случай
-        if (!isPoliclinic && !a3
+     /*   if (!isPoliclinic && !a3
                 && (isOneOf(entry.getFondResult().getCode(), "104", "204"))
                 || entry.getMedHelpProfile().getCode().equals("136") && entry.havePrevMedCase())
             add(z, "VB_P", "1"); // Признак внутрибольничного перевода *05.08 1 - только если есть перевод
+       */
         z.addContent(new Element("SL_TEMPLATE")); // Список случаев
         add(z, "IDSP", entry.getIdsp().getCode()); // Способ оплаты медицинской помощи (V010)
         add(z, "SUMV", entry.getCost()); // Сумма, выставленная к оплате
@@ -236,6 +236,12 @@ public class Expert2XmlServiceBean implements IExpert2XmlService {
      */
     private void add(Element el, String fieldName, Object value) {
         el.addContent(new Element(fieldName).setText(value != null ? value.toString() : ""));
+    }
+    /**
+     * Добавляем в Element значение
+     */
+    private void add(int index, Element el, String fieldName, Object value) {
+        el.addContent(index, new Element(fieldName).setText(value != null ? value.toString() : ""));
     }
 
     /**
@@ -347,7 +353,7 @@ public class Expert2XmlServiceBean implements IExpert2XmlService {
             String isChild = isTrue(entry.getIsChild()) ? "1" : "0";
 
             String[] slIds = entriesString.split(",");
-            Element zSl = createZSl2020(entry, isPoliclinic, slIds.length, cnt, lpuRegNumber
+            Element zSl = createZSl2020(entry, isPoliclinic, cnt, lpuRegNumber
                     , a3);
             int indSl = zSl.indexOf(zSl.getChild("SL_TEMPLATE"));
             Date startHospitalDate = null;
@@ -714,6 +720,10 @@ public class Expert2XmlServiceBean implements IExpert2XmlService {
             if (zSl.getChild("KD_Z") != null) {
                 zSl.getChild("KD_Z").setText(kdz + "");
             }
+            if (isHosp && entry.havePrevMedCase()) {
+                add(zSl.indexOf(zSl.getChild("SL_TEMPLATE")), zSl, "VB_P","1");
+            }
+
             zSl.removeChild("SL_TEMPLATE");
             zap.addContent(zSl); //Добавляем информацию о случае в запись
             return zap;
