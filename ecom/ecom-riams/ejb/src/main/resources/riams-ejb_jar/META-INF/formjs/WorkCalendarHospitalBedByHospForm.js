449677ee3b5c04c032b1dc4720fd11dc42cc3917
milamesher <milamesher@yandex.ru>
2021-06-02 15:36:28 +0400

    #260 fix создание из-под СЛС

--- ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/WorkCalendarHospitalBedByHospForm.js
+++ ecom/ecom-riams/ejb/src/main/resources/riams-ejb_jar/META-INF/formjs/WorkCalendarHospitalBedByHospForm.js
@@ -8,9 +8,6 @@ function onPreSave(aForm, aEntity, aCtx) {
  * Перед сохранением
  */
 function onSave(aForm, aEntity, aCtx) {
-    if (!chekOphtDayEnabled(aForm, aCtx))
-        throw "Этот день недоступен для предварительной госпитализации в офтальмологическое отделение!";
-    checkPatientCountPerDay(aForm, aCtx);
     var date = new java.util.Date();
     aEntity.setEditDate(new java.sql.Date(date.getTime()));
     aEntity.setEditTime(new java.sql.Time(date.getTime()));
@@ -28,11 +25,8 @@ function onCreate(aForm, aEntity, aCtx) {
 }
 
 function onPreCreate(aForm, aCtx) {
-    if (!chekOphtDayEnabled(aForm, aCtx))
-        throw "Этот день недоступен для предварительной госпитализации в офтальмологическое отделение!";
     /*Проверка - есть ли у пациента пред. запись на это число*/
     checkDouble(aForm.patient, aForm.dateFrom, 0, aCtx);
-    checkPatientCountPerDay(aForm, aCtx);
 }
 
 function checkDouble(aPatientId, aDateHosp, aId, aCtx) {
@@ -41,33 +35,4 @@ function checkDouble(aPatientId, aDateHosp, aId, aCtx) {
     if (!aCtx.manager.createNativeQuery(sql).getResultList().isEmpty()) {
         throw "У пациента уже создана предварительная госпитализация на это число, создание еще одной невозможно!";
     }
-}
-
-//Проверка на количество предв. госпитализаций в день
-function checkPatientCountPerDay(aForm, aCtx) {
-    var sql = "select count(pre.id)" +
-        " from WorkCalendarHospitalBed pre" +
-        " left join mislpu lpu on lpu.id=pre.department_id" +
-        " where pre.datefrom = to_date('" + aForm.dateFrom + "','dd.MM.yyyy')" +
-        " and lpu.id='" + aForm.department + "' and lpu.isophthalmic";
-    var cnt = +aCtx.manager.createNativeQuery(sql).getResultList().get(0);
-    var maxCntList = aCtx.manager
-        .createNativeQuery("select keyvalue from softconfig  where key='patientCountPreHospPerDay'")
-        .getResultList();
-    var maxCnt = maxCntList.isEmpty()? 20 : +maxCntList.get(0);
-    if (cnt >= maxCnt)
-            throw "На этот день в этом отделении уже создано максимально допустимое количество ("
-            + maxCnt + ")  предварительных госпитализаций. Выберите другую дату.";
-}
-
-//проверка на доступность дня для плановой госпит. (для офт. отд.)
-function chekOphtDayEnabled(aForm, aCtx) {
-    var sql = "select id from mislpu where id=" + aForm.department + " and isophthalmic";
-    var list = aCtx.manager.createNativeQuery(sql).getResultList();
-    if (list.size()>0) { //офтальмология
-        sql = "select day from datepreopht where day=to_date('" + aForm.dateFrom + "','dd.MM.yyyy')";
-        list = aCtx.manager.createNativeQuery(sql).getResultList();
-        return list.isEmpty();
-    }
-    return true;
 }
