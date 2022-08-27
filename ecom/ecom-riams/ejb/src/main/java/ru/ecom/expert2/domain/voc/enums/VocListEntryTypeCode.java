a7041f2f0e49d3ae892b7a38d35af915e9b93cfc
vtsybulin <vasek007@mail.ru>
2022-08-27 21:35:40 +0400

    ref

--- ecom/ecom-riams/ejb/src/main/java/ru/ecom/expert2/domain/voc/enums/VocListEntryTypeCode.java
+++ ecom/ecom-riams/ejb/src/main/java/ru/ecom/expert2/domain/voc/enums/VocListEntryTypeCode.java
@@ -1,6 +1,7 @@
 package ru.ecom.expert2.domain.voc.enums;
 
 public enum VocListEntryTypeCode {
+    POLYCLINICKDO, //DEPRECATED
     HOSPITAL, //случаи стационарного лечения
     VMP,
     POL_KDP,  //КДП более нету, теперь всё - неотложка. Передалеть на поликлинику
