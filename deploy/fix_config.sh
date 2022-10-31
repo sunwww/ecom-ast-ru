77faccad7e917129db4ef6cccec6f4ba700b9f01
vtsybulin <vasek007@mail.ru>
2022-10-31 23:32:33 +0400

    pipelint build medos

--- deploy/fix_config.sh
+++ deploy/fix_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # put CI envoirment into ant config
 printf "ant-support.prefix.dir=$1/ecom\n" >> deploy/config.properties
-printf "libdir=$1/MedosLibs\n" >> deploy/config.properties
+printf "libdir=$1/MedosLibs/\n" >> deploy/config.properties
 cat deploy/config.properties
 cp deploy/config.properties ecom/ant-support/
